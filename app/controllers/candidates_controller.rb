9055cb49dfb83b0e800933b1ae40ea6af7ea5fd1
Robert O. Alabi <wolo22001@gmail.com>
2024-12-05 21:58:23 +0000

    setup and connect all link in the home page

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,46 +1,67 @@
 class CandidatesController < ApplicationController
   before_action :set_constituency
-  before_action :set_candidate, only: %i[edit update destroy]
+  before_action :set_candidate, only: [:show, :edit, :update, :destroy]
 
-  def new
-    @candidate = @constituency.candidates.new
+  # GET /constituencies/:constituency_id/candidates
+  def index
+    @presidential_candidates = @constituency.candidates.where(candidate_type: 'Presidential')
+    @parliamentary_candidates = @constituency.candidates.where(candidate_type: 'Parliamentary')
   end
 
+  # GET /constituencies/:constituency_id/candidates/new
+  def new
+    @candidate = @constituency.candidates.build
+  end
+
+  # POST /constituencies/:constituency_id/candidates
   def create
-    @candidate = @constituency.candidates.new(candidate_params)
+    @candidate = @constituency.candidates.build(candidate_params)
     if @candidate.save
-      redirect_to @constituency, notice: 'Candidate created successfully.'
+      redirect_to constituency_candidates_path(@constituency), notice: 'Candidate was successfully created.'
     else
       render :new
     end
   end
 
-  def edit; end
+  # GET /constituencies/:constituency_id/candidates/:id
+  def show
+    # @candidate is already set by the before_action
+  end
 
+  # GET /constituencies/:constituency_id/candidates/:id/edit
+  def edit
+    # @candidate is already set by the before_action
+  end
+
+  # PATCH/PUT /constituencies/:constituency_id/candidates/:id
   def update
     if @candidate.update(candidate_params)
-      redirect_to @constituency, notice: 'Candidate updated successfully.'
+      redirect_to constituency_candidate_path(@constituency, @candidate), notice: 'Candidate was successfully updated.'
     else
       render :edit
     end
   end
 
+  # DELETE /constituencies/:constituency_id/candidates/:id
   def destroy
     @candidate.destroy
-    redirect_to @constituency, notice: 'Candidate deleted successfully.'
+    redirect_to constituency_candidates_path(@constituency), notice: 'Candidate was successfully destroyed.'
   end
 
   private
 
+  # Set the constituency for the candidate
   def set_constituency
     @constituency = Constituency.find(params[:constituency_id])
   end
 
+  # Set the candidate to be used for show, edit, update, destroy
   def set_candidate
     @candidate = @constituency.candidates.find(params[:id])
   end
 
+  # Strong parameters to allow only certain attributes for candidates
   def candidate_params
-    params.require(:candidate).permit(:name, :party, :bio)
+    params.require(:candidate).permit(:name, :party, :candidate_type, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   resources :constituencies do
     resources :candidates
   end
